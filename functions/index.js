b78ad74bb22d11cc53726b820255e76d06df6626
keeran2488 <49069135+keeran2488@users.noreply.github.com>
2020-02-05 01:32:22 +0545

    addAssignment function added

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
+exports.addAssignments = functions.firestore
+.document('Assignments/{assignmentID}')
+.onCreate((snap, context) => {
+    console.log("Hello World!");
+    const assignemt = snap.data();
+    const subject = assignemt.Subject;
+    console.log(subject);
+});
